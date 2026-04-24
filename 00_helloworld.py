d56cd7df2c446960b6480e8857416e8b11f88598
Diego <diegoahn0709@gmail.com>
2026-04-24 11:43:51 -0500

    Enlace mi avance en Python con GitHub

---
+++ 00_helloworld.py
@@ -0,0 +1,25 @@
+# Esto es un comentario: Hola mundo
+# Nuestro hola mundo en Python
+print("Hola Python")
+print('Hola Python')
+
+"""
+Este es un
+comentario
+en varias líneas
+"""
+
+'''
+Este es un
+comentario
+en varias líneas
+'''
+
+# Consultar el tipo de dato
+print(type("Soy un dato str")) # Tipo 'str'
+print(type(5)) # Tipo 'int'
+print(type(1.5)) # Tipo 'float'
+print(type(3 + 1j)) # Tipo 'complex'
+print(type(True)) # Tipo 'bool'
+
+print(type(print("Mi cadena de texto"))) # Tipo 'NoneType'
